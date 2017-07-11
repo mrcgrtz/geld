d79e6beaf5db23484fafa8956c566fab292498b4
Marc Görtz <mail@marcgoertz.de>
2017-07-11 21:35:27 +0200

    Add another test for `zeroDecimals`.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,24 @@
 import test from 'ava';
-import format from '../';
+import geld from '../';
 
-test('should format 2.15 as `2,15 €`', t => {
-  t.is(format(2.15), '2,15&nbsp;€');
+test('should format `2.15` as `"2,15 €"` ', t => {
+  t.is(geld(2.15), '2,15&nbsp;€');
 });
 
-test('should format `"2.15"` as `2,15 €`', t => {
-  t.is(format('2.15'), '2,15&nbsp;€');
+test('should format `"2.15"` as `"2,15 €"`', t => {
+  t.is(geld('2.15'), '2,15&nbsp;€');
 });
 
-test('should format `2` as `2 €`', t => {
-  t.is(format(2), '2&nbsp;€');
+test('should format `2` as `"2 €"`', t => {
+  t.is(geld(2), '2&nbsp;€');
 });
 
-test('should format `2000` as `2.000 €`', t => {
-  t.is(format(2000), '2.000&nbsp;€');
+test('should format `2000` as `"2.000 €"`', t => {
+  t.is(geld(2000), '2.000&nbsp;€');
 });
 
-test('should format `"123456.789"` as `$123,456.79`', t => {
-  t.is(format('123456.789', {
+test('should format `"123456.789"` as `"$123,456.79"`', t => {
+  t.is(geld('123456.789', {
     currency: '$',
     currencyPosition: 'before',
     decimalSeparator: '.',
@@ -27,22 +27,28 @@ test('should format `"123456.789"` as `$123,456.79`', t => {
   }), '$123,456.79');
 });
 
-test('should format `987.65` as `988 €`', t => {
-  t.is(format(987.65, {
+test('should format `987.65` as `"988 €"`', t => {
+  t.is(geld(987.65, {
     decimals: 0
   }), '988&nbsp;€');
 });
 
-test('should format `200` as `200,- €`', t => {
-  t.is(format(200, {
+test('should format `200` as `"200,- €"`', t => {
+  t.is(geld(200, {
     zeroDecimals: '-'
   }), '200,-&nbsp;€');
 });
 
+test('should format `200` as `"200,00 €"`', t => {
+  t.is(geld(200, {
+    zeroDecimals: '00'
+  }), '200,00&nbsp;€');
+});
+
 test('should format `two` as empty string', t => {
-  t.is(format('two'), '');
+  t.is(geld('two'), '');
 });
 
 test('should format `null` as empty string', t => {
-  t.is(format(null), '');
+  t.is(geld(null), '');
 });

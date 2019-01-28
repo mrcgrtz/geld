95da1ba3db42c2fd6e7268016df9207dabf518d0
Marc Görtz <mail@marcgoertz.de>
2019-01-28 16:27:24 +0100

    Fix coding style issues.

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 'use strict';
 
-const defaults = {
-  currency: '€',
-  currencyPosition: 'after',
-  decimals: 2,
-  decimalSeparator: ',',
-  orderSeparator: '.',
-  zeroDecimals: '',
-  space: ' '
-};
-
 module.exports = (value, options) => {
-  const config = Object.assign({}, defaults, options);
+	const config = {
+		currency: '€',
+		currencyPosition: 'after',
+		decimals: 2,
+		decimalSeparator: ',',
+		orderSeparator: '.',
+		zeroDecimals: '',
+		space: ' ',
+		...options
+	};
 
-  if (isNaN(value) || value === null) {
-    return '';
-  }
-  value = Number(value);
-  value = value.toFixed(~~config.decimals);
+	if (isNaN(value) || value === null) {
+		return '';
+	}
 
-  const parts = value.split('.');
-  const fnums = parts[0];
-  let dec = parts[1] ? config.decimalSeparator + parts[1] : '';
+	value = Number(value);
+	value = value.toFixed(~~config.decimals);
 
-  let curr = '';
-  if (config.currency) {
-    curr = config.currencyPosition === 'before' ?
-      config.currency + config.space :
-      config.space + config.currency;
-  }
+	const parts = value.split('.');
+	const fnums = parts[0];
+	let dec = parts[1] ? config.decimalSeparator + parts[1] : '';
 
-  // Do something with zero-valued decimals
-  if (parseInt(parts[1], 10) === 0 && config.zeroDecimals !== undefined) {
-    if (config.zeroDecimals === '' || config.zeroDecimals === null) {
-      // Strip away zero-valued decimals
-      dec = '';
-    } else {
-      // Add custom string
-      dec = config.decimalSeparator + config.zeroDecimals;
-    }
-  }
+	let curr = '';
+	if (config.currency) {
+		curr = config.currencyPosition === 'before' ?
+			config.currency + config.space :
+			config.space + config.currency;
+	}
 
-  const formattedValue =
-    fnums.replace(/(\d)(?=(?:\d{3})+$)/g, '$1' + config.orderSeparator) + dec;
-  return config.currencyPosition === 'before' ?
-    // As in '$ 123'
-    curr + formattedValue :
-    // As in '123 €'
-    formattedValue + curr;
+	// Do something with zero-valued decimals
+	if (parseInt(parts[1], 10) === 0 && config.zeroDecimals !== undefined) {
+		if (config.zeroDecimals === '' || config.zeroDecimals === null) {
+			// Strip away zero-valued decimals
+			dec = '';
+		} else {
+			// Add custom string
+			dec = config.decimalSeparator + config.zeroDecimals;
+		}
+	}
+
+	const formattedValue =
+		fnums.replace(/(\d)(?=(?:\d{3})+$)/g, '$1' + config.orderSeparator) + dec;
+	return config.currencyPosition === 'before' ?
+		// As in '$ 123'
+		curr + formattedValue :
+		// As in '123 €'
+		formattedValue + curr;
 };
--- test/test.js
+++ test/test.js
@@ -2,81 +2,81 @@ import test from 'ava';
 import geld from '..';
 
 test('should format `2.15` as `"2,15 €"` ', t => {
-  t.is(geld(2.15), '2,15 €');
+	t.is(geld(2.15), '2,15 €');
 });
 
 test('should format `"2.15"` as `"2,15 €"`', t => {
-  t.is(geld('2.15'), '2,15 €');
+	t.is(geld('2.15'), '2,15 €');
 });
 
 test('should format `2` as `"2 €"`', t => {
-  t.is(geld(2), '2 €');
+	t.is(geld(2), '2 €');
 });
 
 test('should format `2000` as `"2.000 €"`', t => {
-  t.is(geld(2000), '2.000 €');
+	t.is(geld(2000), '2.000 €');
 });
 
 test('should format `"123456.789"` as `"$123,456.79"`', t => {
-  t.is(geld('123456.789', {
-    currency: '$',
-    currencyPosition: 'before',
-    decimalSeparator: '.',
-    orderSeparator: ',',
-    space: ''
-  }), '$123,456.79');
+	t.is(geld('123456.789', {
+		currency: '$',
+		currencyPosition: 'before',
+		decimalSeparator: '.',
+		orderSeparator: ',',
+		space: ''
+	}), '$123,456.79');
 });
 
 test('should format `987.65` as `"988 €"`', t => {
-  t.is(geld(987.65, {
-    decimals: 0
-  }), '988 €');
+	t.is(geld(987.65, {
+		decimals: 0
+	}), '988 €');
 });
 
 test('should format `200` as `"200,- €"`', t => {
-  t.is(geld(200, {
-    zeroDecimals: '-'
-  }), '200,- €');
+	t.is(geld(200, {
+		zeroDecimals: '-'
+	}), '200,- €');
 });
 
 test('should format `200` as `"200,00 €"`', t => {
-  t.is(geld(200, {
-    zeroDecimals: '00'
-  }), '200,00 €');
+	t.is(geld(200, {
+		zeroDecimals: '00'
+	}), '200,00 €');
 });
 
 test('should format `200` as `"200 €"` with `zeroDecimals` set to an empty string', t => {
-  t.is(geld(200, {
-    zeroDecimals: ''
-  }), '200 €');
+	t.is(geld(200, {
+		zeroDecimals: ''
+	}), '200 €');
 });
 
 test('should format `200` as `"200 €"` with `zeroDecimals` set to `null`', t => {
-  t.is(geld(200, {
-    zeroDecimals: null
-  }), '200 €');
+	t.is(geld(200, {
+		zeroDecimals: null
+	}), '200 €');
 });
 
 test('should format `200` as `"200" when no currency is provided`', t => {
-  t.is(geld(200, {
-    currency: null
-  }), '200');
+	t.is(geld(200, {
+		currency: null
+	}), '200');
 });
 
 test('should format `200` as `"200&nbsp;€" when a non-breaking space entity is provided`', t => {
-  t.is(geld(200, {
-    space: '&nbsp;'
-  }), '200&nbsp;€');
+	t.is(geld(200, {
+		space: '&nbsp;'
+	}), '200&nbsp;€');
 });
 
 test('should format `two` as empty string', t => {
-  t.is(geld('two'), '');
+	t.is(geld('two'), '');
 });
 
 test('should format `null` as empty string', t => {
-  t.is(geld(null), '');
+	t.is(geld(null), '');
 });
 
 test('should format `undefined` as empty string', t => {
-  t.is(geld(undefined), '');
+	t.is(geld(undefined), '');
 });
